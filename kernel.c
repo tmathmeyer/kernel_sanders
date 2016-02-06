c68f4581a07da61586174c7c657914c74d5c93a0
roboman2444 <roboman2444@gmail.com>
2016-02-06 14:41:22 -0500

    screentext basic

--- kernel.c
+++ kernel.c
@@ -1,15 +1,13 @@
 #include "keyboard_map.h"
+#include "screentext.h"
 
-#define LINES 25
-#define COLUMNS_IN_LINE 80
-#define BYTES_FOR_EACH_ELEMENT 2
-#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
 #define KEYBOARD_DATA_PORT 0x60
 #define KEYBOARD_STATUS_PORT 0x64
 #define IDT_SIZE 256
 #define INTERRUPT_GATE 0x8e
 #define KERNEL_CODE_SEGMENT_OFFSET 0x08
 #define ENTER_KEY_CODE 0x1C
+#define BACKSPACE_KEY_CODE 0x0E
 
 extern unsigned char keyboard_map[128];
 extern void keyboard_handler(void);
@@ -17,8 +15,6 @@ extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
 extern void load_idt(unsigned long *idt_ptr);
 
-unsigned int current_loc = 0; // cursor
-char *vidptr = (char*)0xb8000; //for vidmemory
 
 struct IDT_entry {
     unsigned short int offset_lowerbits;
@@ -60,27 +56,6 @@ void kb_init(void) {
     write_port(0x21 , 0xFD); /* enable keyboard */
 }
 
-void kprint(const char *str) {
-    unsigned int i = 0;
-    while (str[i] != '\0') {
-        vidptr[current_loc++] = str[i++];
-        vidptr[current_loc++] = 0x07;
-    }
-}
-
-void kprint_newline(void) {
-    unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-    current_loc = current_loc + (line_size - current_loc % (line_size));
-}
-
-void clear_screen(void) {
-    unsigned int i = 0;
-    while (i < SCREENSIZE) {
-        vidptr[i++] = ' ';
-        vidptr[i++] = 0x07;
-    }
-}
-
 void keyboard_handler_main(void) {
     unsigned char status;
     char keycode;
@@ -95,17 +70,19 @@ void keyboard_handler_main(void) {
             return;
 
         if(keycode == ENTER_KEY_CODE) {
-            kprint_newline();
+            screentext_newline();
             return;
         }
-
-        vidptr[current_loc++] = keyboard_map[(unsigned char) keycode];
-        vidptr[current_loc++] = 0x07;
+        if(keycode == BACKSPACE_KEY_CODE) {
+            screentext_backspace();
+            return;
+        }
+        screentext_writechar(keyboard_map[(unsigned char) keycode]);
     }
 }
 
 void kmain(void) {
-    clear_screen();
+    screentext_clear();
     idt_init();
     kb_init();
     while(1);
---
+++ screentext.c
@@ -0,0 +1,49 @@
+#include "screentext.h"
+
+
+
+int screen_cursorx = 0;
+int screen_cursory = 0;
+
+
+int vid_lines = 25;
+int vid_col = 80;
+int vid_bpc = 2;
+
+char * vidptr = (char*) 0xb8000;
+
+
+void screentext_clear(void){
+	unsigned int ss = vid_lines * vid_col;
+	unsigned int i;
+	for(i = 0; i < ss; i++){
+		((unsigned short *) vidptr)[i] = 0x0720;
+	}
+}
+
+void screentext_newline(void){
+	screen_cursorx = 0;
+	screen_cursory = (screen_cursory+1) % vid_lines;
+}
+
+void screentext_backspace(void){
+	screen_cursorx--;
+	if(screen_cursorx < 0){
+		screen_cursorx = vid_col-1;
+		screen_cursory = screen_cursory ? screen_cursory-1 : 0;
+	}
+	screentext_writecharplace(' ', screen_cursorx, screen_cursory);
+}
+
+void screentext_writecharplace(char c, int x, int y){
+	vidptr[(y * vid_col + x)*vid_bpc] = c;
+	vidptr[(y * vid_col + x)*vid_bpc+1] = 0x07;
+}
+void screentext_writechar(char c){
+	screentext_writecharplace(c, screen_cursorx, screen_cursory);
+	screen_cursorx ++;
+	screen_cursory += screen_cursorx/vid_col;
+	screen_cursorx %= vid_col;
+	screen_cursory %= vid_lines;
+}
+
