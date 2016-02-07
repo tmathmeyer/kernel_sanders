356cbe7e1c21db3041b830b08d32ee1039dcc048
Ted Meyer <tmathmeyer@gmail.com>
2016-02-07 09:50:08 -0500

    hopefully it works

--- the_iowa_caucus.c
+++ the_iowa_caucus.c
@@ -97,8 +97,6 @@ int randdir(int x, int y) {
 
 void color(unsigned char* dest, unsigned char* src) {
     *dest = ((*src - GRADIENT_START + 1) % (256-GRADIENT_START)) + GRADIENT_START;
-    // *dest = (*src + 1) % 256;
-    // *dest = 130;
 }
 
 extern int init_vga(int blah);
@@ -106,63 +104,66 @@ extern int init_vga(int blah);
 int the_iowa_caucus(int argc, char *argv[]) {
     init_vga(0);
 
-    pixels_back = mm_alloc(VIDEO_WIDTH * VIDEO_HEIGHT);
-
-    int i; 
-    for (i = 0; i < VIDEO_WIDTH * VIDEO_HEIGHT; i++) {
-        *(pixels_back + i) = 0;
-    }
-
-    int x = sand_rand() % VIDEO_WIDTH;
-    int y = sand_rand() % VIDEO_HEIGHT;
-
-    *p(x, y) = sand_rand() % (256 - GRADIENT_START) + GRADIENT_START;
-    
-    *pb(x, y) = 5;
-
-    int write = 0;
-    do {
-        int z = randdir(x,y);
-        if (z) {write++;}
-        switch(z) {
-            case 0:
-                switch(*pb(x,y)) {
-                    case 5:
-                        the_iowa_caucus_exit();
-                        return 0;
-                    case 1:
-                        y--; break;
-                    case 2:
-                        x++; break;
-                    case 3:
-                        y++; break;
-                    case 4:
-                        x--; break;
-                }
-                break;
-            case 1: // go up
-                color(p(x,y-1), p(x,y));
-                y--;
-                *pb(x,y) = 3;
-                break;
-            case 3: // go down
-                color(p(x,y+1), p(x,y));
-                y++;
-                *pb(x,y) = 1;
-                break;
-            case 2: // go right
-                color(p(x+1,y), p(x,y));
-                x++;
-                *pb(x,y) = 4;
-                break;
-            case 4: // go left
-                color(p(x-1,y), p(x,y));
-                x--;
-                *pb(x,y) = 2;
-                break;
+    while(1) {
+        pixels_back = mm_alloc(VIDEO_WIDTH * VIDEO_HEIGHT);
+        int i; 
+        for (i = 0; i < VIDEO_WIDTH * VIDEO_HEIGHT; i++) {
+            *(pixels_back + i) = 0;
+        }
+        int x = sand_rand() % VIDEO_WIDTH;
+        int y = sand_rand() % VIDEO_HEIGHT;
+        *p(x, y) = sand_rand() % (256 - GRADIENT_START) + GRADIENT_START;
+        *pb(x, y) = 5;
+        int write = 0;
+        do {
+            int z = randdir(x,y);
+            if (z) {write++;}
+            switch(z) {
+                case 0:
+                    switch(*pb(x,y)) {
+                        case 5:
+                            the_iowa_caucus_exit();
+                            return 0;
+                        case 1:
+                            y--; break;
+                        case 2:
+                            x++; break;
+                        case 3:
+                            y++; break;
+                        case 4:
+                            x--; break;
+                    }
+                    break;
+                case 1: // go up
+                    color(p(x,y-1), p(x,y));
+                    y--;
+                    *pb(x,y) = 3;
+                    break;
+                case 3: // go down
+                    color(p(x,y+1), p(x,y));
+                    y++;
+                    *pb(x,y) = 1;
+                    break;
+                case 2: // go right
+                    color(p(x+1,y), p(x,y));
+                    x++;
+                    *pb(x,y) = 4;
+                    break;
+                case 4: // go left
+                    color(p(x-1,y), p(x,y));
+                    x--;
+                    *pb(x,y) = 2;
+                    break;
+            }
+            for(int i = 0; i<10000; i++);
+        }
+        while(*pb(x,y) != 5);
+        
+        for (i = 0; i < VIDEO_WIDTH * VIDEO_HEIGHT; i++) {
+            *(pixels_back + i) = 0;
+            color(p(i,0), 0);
         }
     }
-    while(*pb(x,y) != 5);
     the_iowa_caucus_exit();
     return 0;
 }
