452d27f096647368eea5e2471637f2e93ee65f5e
Jonas <jprmesh@gmail.com>
2016-02-06 20:28:21 -0500

    First bits of sanders text editor (si).

---
+++ si.c
@@ -0,0 +1,39 @@
+#include "si.h"
+
+void* fopen(char* filename);
+int getflen(char* filename);
+int fclose(char* filename);
+
+si_status status;
+
+void si_openfile(char* filename) {
+    string_copy(status.filename, filename);
+    status.fileaddr = fopen(filename);
+    status.filelen = getflen(filename);
+    screentext_clear();
+    update_cursorxy(0,0);
+}
+
+void si_readfile(void) {
+    status.buffer = mm_alloc(status.filelen);
+    mm_copy(status.buffer, status.fileaddr, status.filelen);
+}
+
+void si_displayfile(void) {
+    char* nextchar = status.buffer;
+    while(1) {
+        if (*nextchar == 0) break;
+        screentext_writecharnc(*nextchar);
+        nextchar++;
+    }
+    update_cursorxy(0,0);
+}
+
+void si_writefile(void) {
+    ////////
+}
+
+void si_closefile(void) {
+    fclose(status.filename);
+    mm_free(status.buffer);
+}
---
+++ si.h
@@ -0,0 +1,28 @@
+#ifndef __si_h__
+#define __si_h__
+
+#include "screentext.h"
+#include "alloc.h"
+#include "screentext.h"
+
+typedef enum si_mode {
+    INSERT,
+    EXECUTE
+} si_mode;
+
+typedef struct {
+    si_mode mode;
+    int cursorloc;
+    char* filename;
+    void* fileaddr;
+    int filelen;
+    void* buffer;
+} si_status;
+
+void si_openfile(char* filename);
+void si_readfile(void);
+void si_displayfile(void);
+void si_writefile(void);
+void si_closefile(void);
+
+#endif // __si_h__
