c6b794934d0cb8a7df10cd0280a65566d17edf80
roboman2444 <roboman2444@gmail.com>
2016-02-07 07:17:07 -0500

    spinnycubes

--- video.c
+++ video.c
@@ -5,6 +5,7 @@
 #include "boopt.h"
 #include "sleep.h"
 #include <math.h>
+#include "matrixlib.h"
 // LOTS OF TODO HERE
 
 int vid_x;
@@ -175,13 +176,86 @@ void video_draw_triangle(unsigned int color, ivec_t *verts, void * tridata){
 void video_key_handler(char keycodde){
 	return;
 }
+
+
+matrix4x4_t proj;
+vec_t  verts[24] = {
+-1.0, -1.0, -1.0,
+-1.0, -1.0, 1.0,
+-1.0, 1.0, -1.0,
+-1.0, 1.0, 1.0,
+1.0, -1.0, -1.0,
+1.0, -1.0, 1.0,
+1.0, 1.0, -1.0,
+1.0, 1.0, 1.0 };
+int indices[24] = { 0, 1, 0, 2, 1, 3, 2, 3, 4, 5, 4, 6, 5, 7, 6, 7,
+0, 4, 1, 5, 2, 6, 3, 7};
+
+int oldc1[48] = {0};
+int newc1[48] ={0};
+int oldc2[48] = {0};
+int newc2[48] ={0};
+void calc_cube(int * buf, float spinsx, float spinsy, float spinsz, int centx, int centy, float scaler){
+	matrix4x4_t meet;
+	matrix4x4_t mvp;
+	Matrix4x4_CreateFromQuakeEntity(&meet, 0.0, 0.0, 3.0, spinsx, spinsy, spinsy, 1.0);
+	Matrix4x4_Concat(&mvp, &proj, &meet);
+	int i;
+
+	for( i = 0; i < 12; i++){
+		vec_t * pp = &verts[indices[i*2] * 3];
+		vec_t * ps = &verts[indices[i*2+1] * 3];
+//		vec_t * pp = &verts[0];
+//		vec_t * ps = &verts[3];
+		vec4_t in1, out1, in2, out2;
+		in1[0] = pp[0];
+		in1[1] = pp[1];
+		in1[2] = pp[2];
+		in1[3] = 1.0;
+		in2[0] = ps[0];
+		in2[1] = ps[1];
+		in2[2] = ps[2];
+		in2[3] = 1.0;
+		Matrix4x4_Transform4(&mvp, in1, out1);
+		Matrix4x4_Transform4(&mvp, in2, out2);
+		out1[0] /= out1[3];
+		out1[1] /= out1[3];
+		out2[0] /= out2[3];
+		out2[1] /= out2[3];
+		//now "clip" to screen coords
+		int x1, x2, y1, y2;
+		buf[i * 4] = out1[0] * scaler+centx;
+		buf[i * 4+1] = out1[1] * scaler+centy;
+		buf[i * 4+2] = out2[0] * scaler+centx;
+		buf[i * 4+3] = out2[1] * scaler+centy;
+	}
+
+}
+void draw_cube(char color, int * old, int * new){
+	int i = 0;
+	for(i = 0; i < 12; i++){
+		line(0, old[i*4], old[i*4+1], old[i*4+2], old[i*4+3]);
+	}
+	for(i = 0; i < 12; i++){
+		line(color,new[i*4], new[i*4+1], new[i*4+2], new[i*4+3]);
+	}
+}
+
 extern int init_vga(int blah);
 int videorun(int argc, char * argv[]){
 	set_keyboard_handler(&video_key_handler);
 	init_vga(0);
 	vid_x = 320;
 	vid_y = 200;
+
+	float dz = 10.0 - 0.1;
+	proj.m[0][0] = 1.0 / ((float)vid_x / (float)vid_y);
+	proj.m[1][1] = 1.0;
+	proj.m[2][2] = -(10.0 + 0.1) / dz;
+	proj.m[2][3] = -1.0;
+	proj.m[3][2] = -2.0 * 0.1 * 10.0 / dz;
 	int i,z;
+	int jj = 0;
 	/*
 	for(i = 0; i < 320 * 200; i++){
 		vidmem[i] = (i*123) % 255;
@@ -200,6 +274,15 @@ int videorun(int argc, char * argv[]){
 		float yoff = sin(i * 0.001);
 		float cent = sin(i * 0.00131 + 0.00313) * 20.0;
 		line(i/ 1000 % 39, 160 + xoff * cent, 100 + yoff * cent, 160 + xoff * (40.0 + cent * 0.1), 100 + yoff * (40 + cent * 0.1));
+		if(!(i % 10)){
+			jj = !jj;
+			calc_cube( jj ? newc1 : oldc1, i * 0.01, sin(i * 0.00011) * 500.0,0.0, 70, 100, 100.0);
+			calc_cube( jj ? newc2 : oldc2, i * 0.01 + 30.0, sin(i * 0.000051) * 1000.0,0.0, 250, 100, 100.0);
+			if(jj) draw_cube(6, oldc1, newc1);
+			else draw_cube(6, newc1, oldc1);
+			if(jj) draw_cube(10, oldc2, newc2);
+			else draw_cube(10, newc2, oldc2);
+		}
 		sleepy_sanders(10000);
 	}
 //	sanders_printf("video mode is %i\n", get_mode());
